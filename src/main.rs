5852e264f96bbc9008d2349476e27d8efed534e2
Anthony Butcher <anthony.butcher@samblagroup.com>
2022-03-29 12:19:44 +0200

    What the rust?!

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use std::env::{args, Args};
+
+fn main() {
+    let mut args: Args = args();
+
+    let first = args.nth(1).unwrap();
+    let operator = args.nth(0).unwrap().chars().next().unwrap();
+    let second = args.nth(0).unwrap();
+
+    let first_number = first.parse::<f32>().unwrap();
+    let second_number = second.parse::<f32>().unwrap();
+
+    let result = operatate(operator, first_number, second_number);
+
+    println!("{:?}", output(first_number, operator, second_number, result));
+}
+
+fn operatate(operator: char, first_number: f32, second_number: f32) -> f32 {
+     match operator {
+          '+' => first_number + second_number,
+          '-' => first_number - second_number,
+          '/' => first_number / second_number,
+          '*' | 'x' | 'X' => first_number * second_number,
+          _ => panic!("invalid operator used!")
+     }
+}
+
+fn output(first_number: f32, operatator: char, second_number: f32, result: f32) -> String {
+    format!("{} {} {} = {}", first_number, operatator, second_number, result)
+}
